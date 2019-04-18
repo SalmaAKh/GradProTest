45173c2bcb7dd3595735c03bf4bc9fca5f5b1f8c
noor2 <nooor.ahmed20177@gmail.com>
2019-04-18 14:52:01 +0300

    intialization of front end

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,64 @@
 <?php
 
-use Illuminate\Http\Request;
+  
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('admin')->group(function () {
+    
+    
+    
+    
+    
+    Route::get('offered_course/browser', function () {
+     
+        
+        return view('offered_course.browser');
+
+        
+        
+        
+    });
+        
+    
+    Route::get('offered_course/new', function () {
+     
+        
+        return view('offered_course.new');
+
+        
+        
+        
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 });
+
+
+
